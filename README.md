6f28be1b48a321c3cb9c14258b5d761d30a9171c
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-09-03 18:05:45 -0400

    Major refactor

--- README.md
+++ README.md
@@ -1,21 +1,14 @@
 # dspace-osti 
 
-For oversight reasons, [OSTI](https://www.osti.gov/) requires that PPPL submit its datasets' metadata through their API. OSTI is only a metadata repository, and the datasets themselves are stored in Dataspace. We are responsible for posting the metadata by the end of each fiscal year. This is not to be confused with submitting journal article metadata to contact OSTI, which is an entirely separate process and is handled by PPPL.
-
-## Useful Links:
-
-- [OSTI API](https://www.osti.gov/elink/241-6api.jsp)
-- [Previously submitted PPPL datasets](https://www.osti.gov/dataexplorer/api/v1/records?site_ownership_code=PPPL)
-- [PPPL DSpace Community](https://dataspace.princeton.edu/handle/88435/dsp01pz50gz45g)
-- [DSpace REST Documentation](https://dataspace.princeton.edu/rest/)
-- [OSTI's old code](https://github.com/doecode/dspace)
-
-## Get an ELink account
-
-To post to OSTI, a user needs to acquire an ELink account. Currently, the OSTI API does not have a UI to create an account, so a new user will have to OSTI directly.
+For oversight reasons, [OSTI](https://www.osti.gov/) requires that PPPL submit its datasets' metadata through their API. OSTI is only a metadata repository, and the datasets themselves are stored in Dataspace. We are responsible for posting the metadata by the end of each fiscal year. This is not to be confused with submitting journal article metadata to OSTI, which is an entirely separate process and is handled by PPPL.
 
 ## Setup
 
+### Get an ELink account
+
+To post to OSTI, a user needs to acquire an ELink account. Currently, the OSTI API does not have a UI to create an account, so a new user will have to OSTI directly.
+
+### Setup an environment
 `ostiapi` looks for two environment variables, `OSTI_USERNAME` and `OSTI_PASSWORD`. After a user gets an ELink Account, one can set the appropriate variables in `secrets.sh`, which is already removed from version control by `.gitignore`.
 
 ```
@@ -29,31 +22,27 @@ We are dependent on [ostiapi](https://github.com/doecode/ostiapi) as a submodule
 pip install -r requirements.txt
 ```
 
-## Pipeline
+## Workflow
 
 ### Pull necessary data
 
-#### get_existing_datasets.py
-Pull the JSON of datasets we've already pushed to OSTI in previous fiscal years. Output `existing_osti_datasets.json`
+Run `python Scraper.py` to collect data from OSTI & DSpace. The pipeline will compare (by title) to see which datasets haven't yet been uploaded. It will output an form `entry_form.csv` that one needs to manually fill out with DOE Contract information 
 
-#### get_dspace_metadata.py
-Pull the JSON of datasets we currently host on Dataspace. Output `dspace_dataset_metadata.json`
+### Manually enter data
 
-#### get_unposted_metadata.py
-Find the difference between `existing_osti_datasets.json` and `dspace_datasets.json`, and output `dataset_metadata_to_upload.json`.
+Move `entry_form.csv` into a Google Sheet and share with partners at PPPL. They will need to enter `Sponsoring Organizations`, `DOE Contract`, and `Datatype`. [See `Datatype` codes here.](https://github.com/doecode/ostiapi#data-set-content-type-values) (Ideally, in the long run we would integrate these fields into DSpace's metadata.) Save the file into this folder as `form_input.csv`.
 
+Note: Since we're joining by title, typos and encoding errors will inevitably lead to missed results. `Scraper.py` also checks for items that are in OSTI but not DSpace, something that shouldn't happen. The user will need to manually remove those rows from the entry form.
 
-### Prepare for publishing
-Some metadata required by OSTI is not found in the DSpace metadata. Therefore, a spreadsheeet needs to be filled out with the appropriate information.
+### Post data to OSTI
 
-#### generate_contract_entry_form.py
-Generate `contract_info.csv` that can be used to collect federal contract numbers and sponsor organizations.
-
-#### generate_upload_json.py
-Given all data files mentioned above, create the final JSON, `osti.json`, which will be used to post to OSTI.
+Run `python Poster.py` to use the `form_input.csv` and DSpace metadata to generate the JSON necessary for OSTI ingestion. It then posts to OSTI using their API.
 
 
-### Post to OSTI
-#### post_to_osti.py
-Use the OSTI API to post `osti.json`.
+## Useful Links:
 
+- [OSTI API](https://www.osti.gov/elink/241-6api.jsp)
+- [Previously submitted PPPL datasets](https://www.osti.gov/dataexplorer/api/v1/records?site_ownership_code=PPPL)
+- [PPPL DSpace Community](https://dataspace.princeton.edu/handle/88435/dsp01pz50gz45g)
+- [DSpace REST Documentation](https://dataspace.princeton.edu/rest/)
+- [OSTI's old code](https://github.com/doecode/dspace)
---
+++ Scraper.py
@@ -0,0 +1,141 @@
+import os
+import json
+import html
+
+import requests
+import pandas as pd
+
+from os.path import join as pjoin
+
+class Scraper:
+    """Pipeline to collect data from OSTI & DSpace, comparing which datasets
+     are not yet posted, and generating a form for a user to manually enter
+     additional needed information."""
+    def __init__(self, data_dir='data', osti_scrape='osti_scrape.json', 
+            dspace_scrape='dspace_scrape.json', entry_form_full_path='entry_form.csv', 
+            to_upload='dataset_metadata_to_upload.json'):
+
+        data_dir = 'data'
+        self.osti_scrape = pjoin(data_dir, osti_scrape)
+        self.dspace_scrape = pjoin(data_dir, dspace_scrape)
+        self.entry_form = entry_form
+        self.to_upload = os.path.join(data_dir, to_upload)
+
+        if not os.path.exists(data_dir):
+            os.mkdir(data_dir)
+
+
+    def get_existing_datasets(self):
+        """Paginate through OSTI's Data Explorer API to find the datasets that have
+        already been submitted.
+        """
+        MAX_PAGE_COUNT = 10
+        existing_datasets = []
+
+        for page in range(MAX_PAGE_COUNT):
+            r = requests.get(f'https://www.osti.gov/dataexplorer/api/v1/records?site_ownership_code=PPPL&page={page}')
+            j = json.loads(r.text)
+            if len(j) != 0:
+                existing_datasets.extend(j)
+            else:
+                print(f'Pulled {len(existing_datasets)} records from OSTI.')
+                break
+        else:
+            raise BaseException("Didn't reach the final page of OSTI! Increase the variable MAX_PAGE_COUNT")
+
+        with open(self.osti_scrape, 'w') as f:
+            json.dump(existing_datasets, f, indent=4)
+
+    
+    def get_dspace_metadata(self):
+        """Collect metadata on all items from all DSpace PPPL collections.
+        
+        collections = {
+            'Plasma Science & Technology': 1422,
+            'Theory and Computation': 2266,
+            'Stellarators': 1308,    
+            'NSTX': 1282,
+            'NSTX-U': 1304
+        }
+        """
+        collection_ids = [1422, 2266, 1308, 1282, 1304]
+        all_items = []
+
+        for collection_id in collection_ids:
+            r = requests.get(
+                f'https://dataspace.princeton.edu/rest/collections/{collection_id}/items?expand=metadata', 
+                verify=False
+            )
+            j = json.loads(r.text)
+            all_items.extend(j)
+
+        print(f'Pulled {len(all_items)} records from DSpace.\n\n')
+        with open(self.dspace_scrape, 'w') as f:
+            json.dump(all_items, f, indent=4)
+
+
+    def get_unposted_metadata(self):
+        """Compare the OSTI and DSpace JSON to see what titles need to be uploaded.
+        """
+
+        with open(self.dspace_scrape) as f:
+            dspace_j = json.load(f)
+        with open(self.osti_scrape) as f:
+            osti_j = json.load(f)
+
+        osti_titles = [html.unescape(x['title']) for x in osti_j]
+        dspace_titles = [x['name'] for x in dspace_j]
+
+        titles_to_be_published = [x for x in dspace_titles if x not in osti_titles]
+        to_be_published = [item for item in dspace_j if item['name'] in titles_to_be_published]
+
+        with open(self.to_upload, 'w') as f:
+            json.dump(to_be_published, f, indent=4)
+
+        print(f"{len(to_be_published)} unpublished records were found.", end="\n\n")
+        
+
+        errors = [x for x in osti_titles if x not in dspace_titles]
+        if len(errors) > 0:
+            print(f"The following records were found on OSTI but not in DSpace (that" + 
+                " shouldn't happen). If they closely resemble records we are about to" +
+                " upload, please remove those records from from the upload process.")
+            for error in errors:
+                print("---  " + error)
+
+
+    def generate_contract_entry_form(self):
+        """Create a CSV where a user can enter Sponsoring Organizations, DOE
+         Contract, and Datatype, additional information required by OSTI
+        """
+        with open(self.to_upload) as f:
+            to_upload_j = json.load(f)
+
+
+        df = pd.DataFrame()
+        df['DSpace ID'] = [item['id'] for item in to_upload_j]
+        df['Issue Date'] = [[m['value'] for m in item['metadata'] if m['key'] == 'dc.date.issued'][0] for item in to_upload_j]
+        df['Title'] = [item['name'] for item in to_upload_j]
+        df['Author'] = [';'.join([m['value'] for m in item['metadata'] if m['key'] == 'dc.contributor.author']) for item in to_upload_j]
+        df['Dataspace Link'] = ["https://dataspace.princeton.edu/handle/" + item['handle'] for item in to_upload_j]
+
+        # To be filled in:
+        df['Sponsoring Organizations'] = None
+        df['DOE Contract'] = None
+        df['Datatype'] = None
+
+        df = df.sort_values('Issue Date')
+        df.to_csv(self.entry_form, index=False)
+
+
+    def run_pipeline(self, scrape=True):
+        if scrape:
+            self.get_existing_datasets()
+            self.get_dspace_metadata()
+        self.get_unposted_metadata()
+        self.generate_contract_entry_form()
+
+
+if __name__ == '__main__':
+    s = Scraper()
+    s.run_pipeline()
