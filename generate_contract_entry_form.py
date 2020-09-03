a5bf89150605caf870410bdb4354db2d12200c79
Kevin McElwee <kevinrmcelwee@gmail.com>
2020-09-03 13:25:20 -0400

    Fix URL error

--- generate_contract_entry_form.py
+++ generate_contract_entry_form.py
@@ -16,7 +16,7 @@ def generate_contract_entry_form():
     df['Issue Date'] = [[m['value'] for m in item['metadata'] if m['key'] == 'dc.date.issued'][0] for item in to_upload_j]
     df['Title'] = [item['name'] for item in to_upload_j]
     df['Author'] = [';'.join([m['value'] for m in item['metadata'] if m['key'] == 'dc.contributor.author']) for item in to_upload_j]
-    df['Dataspace Link'] = ["https://dataspace.princeton.edu/handle" + item['handle'] for item in to_upload_j]
+    df['Dataspace Link'] = ["https://dataspace.princeton.edu/handle/" + item['handle'] for item in to_upload_j]
 
     # To be filled in:
     df['Sponsoring Organizations'] = None
