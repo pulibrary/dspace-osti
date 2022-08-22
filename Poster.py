12f9e083c65ad766a0f7015ac8e4020886988764
Chun Ly <astro.chun@gmail.com>
2022-08-22 12:51:04 -0700

    Clean up Poster for PEP8 and other code format warnings

--- Poster.py
+++ Poster.py
@@ -1,18 +1,20 @@
+import datetime
 import json
 import os
-import datetime
-import argparse
 import sys
 
 import pandas as pd
 
 import ostiapi
 
+
 class Poster:
     """Use the form input and DSpace metadata to generate the JSON necessary
      for OSTI ingestion. Then post to OSTI using their API"""
-    def __init__(self, mode, data_dir='data', to_upload='dataset_metadata_to_upload.json',
-     form_input_full_path='form_input.tsv', osti_upload='osti.json', response_dir="responses"):
+    def __init__(
+        self, mode, data_dir='data', to_upload='dataset_metadata_to_upload.json',
+        form_input_full_path='form_input.tsv', osti_upload='osti.json', response_dir="responses"
+    ):
         self.mode = mode
 
         # Prepare all paths
@@ -21,8 +23,10 @@ class Poster:
         self.to_upload = os.path.join(data_dir, to_upload)
         self.osti_upload = os.path.join(data_dir, osti_upload)
 
-        self.response_output = os.path.join(response_dir,
-            f"{mode}_osti_response_{str(datetime.datetime.now()).replace(':', '')}.json")
+        self.response_output = os.path.join(
+            response_dir,
+            f"{mode}_osti_response_{str(datetime.datetime.now()).replace(':', '')}.json"
+        )
 
         assert os.path.exists(data_dir)
         assert os.path.exists(response_dir)
@@ -61,13 +65,19 @@ class Poster:
             return series.shape[0] == series.dropna().shape[0]
 
         expected_columns = ['Sponsoring Organizations', 'DOE Contract', 'Datatype']
-        assert all([col in df.columns for col in expected_columns]), f"You're missing one of these columns {expected_columns}"
-        assert no_empty_cells(df['Sponsoring Organizations']), f"You have empty values in the Sponsoring Organizations column. This is a required value."
-        assert no_empty_cells(df['DOE Contract']), f"You have empty values in the DOE Contract column. This is a required value."
-        assert no_empty_cells(df['Datatype']), f"You have empty values in the Datatype column. This is a required value."
+        assert all([col in df.columns for col in expected_columns]), \
+            f"You're missing one of these columns {expected_columns}"
+        assert no_empty_cells(df['Sponsoring Organizations']), \
+            f"You have empty values in the Sponsoring Organizations column. This is a required value."
+        assert no_empty_cells(df['DOE Contract']), \
+            f"You have empty values in the DOE Contract column. This is a required value."
+        assert no_empty_cells(df['Datatype']), \
+            f"You have empty values in the Datatype column. This is a required value."
 
-        accepted_datatype_values = ['AS','GD', 'IM', 'ND', 'IP', 'FP', 'SM', 'MM', 'I']
-        assert all([dt in accepted_datatype_values for dt in df['Datatype']]), f"The Datatype column contains improper datatype values. The accepted datatype values are: {accepted_datatype_values}"
+        accepted_datatype_values = ['AS', 'GD', 'IM', 'ND', 'IP', 'FP', 'SM', 'MM', 'I']
+        assert all([dt in accepted_datatype_values for dt in df['Datatype']]), \
+            "The Datatype column contains improper datatype values. " \
+            f"The accepted datatype values are: {accepted_datatype_values}"
 
         # Generate final JSON to post to OSTI
         osti_format = []
@@ -86,14 +96,20 @@ class Poster:
             # Collect all required information
             item_dict = {
                 'title': dspace_data['name'],
-                'creators': ';'.join([m['value'] for m in dspace_data['metadata'] if m['key'] == 'dc.contributor.author']),
+                'creators': ';'.join(
+                    [
+                        m['value']
+                        for m in dspace_data['metadata']
+                        if m['key'] == 'dc.contributor.author'
+                    ]
+                ),
                 'dataset_type': row['Datatype'],
                 'site_url': "https://dataspace.princeton.edu/handle/" + dspace_data['handle'],
                 'contract_nos': row['DOE Contract'],
                 'sponsor_org': row['Sponsoring Organizations'],
                 'research_org': 'PPPL',
                 'accession_num': dspace_data['handle'],
-                'publication_date': pub_date
+                'publication_date': pub_date,
             }
 
             # Collect optional required information
@@ -121,8 +137,8 @@ class Poster:
         with open(self.osti_upload, 'w') as f:
             json.dump(osti_format, f, indent=4)
 
-
-    def _fake_post(self, records, username, password):
+    @staticmethod
+    def _fake_post(records):
         """A fake JSON response that mirrors OSTI's"""
         return {
             "record": [
@@ -142,7 +158,6 @@ class Poster:
             ]
         }
 
-
     def post_to_osti(self):
         """Post the collected metadata to OSTI's test or prod server. If in
          dry-run mode, call our _fake_post method"""
@@ -154,7 +169,7 @@ class Poster:
 
         print('Posting data...')
         if self.mode == 'dry-run':
-            response_data = self._fake_post(osti_j, self.username, self.password)
+            response_data = self._fake_post(osti_j)
         else:
             response_data = ostiapi.post(osti_j, self.username, self.password)
 
@@ -172,10 +187,11 @@ class Poster:
             if all([item['status'] == 'SUCCESS' for item in response_data['record']]):
                 print("Congrats 🚀 OSTI says that all records were successfully uploaded!")
             else:
-                print("Some of OSTI's responses do not have 'SUCCESS' as their" +
+                print(
+                    "Some of OSTI's responses do not have 'SUCCESS' as their" +
                     f" status. Look at the file {self.response_output} to" +
-                    " see which records were not successfully uploaded.")
-
+                    " see which records were not successfully uploaded."
+                )
 
     def run_pipeline(self):
         self.generate_upload_json()
